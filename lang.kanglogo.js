68b06289ded4e7949850847a361398a07ccf90e4
Agus Triana <agustriana289@gmail.com>
2023-10-08 22:30:57 +0700

    Create lang.kanglogo.js

---
+++ lang.kanglogo.js
@@ -0,0 +1,188 @@
+
+var arrLang = {
+  "en-gb": {
+    "HOME": "Home",
+    "FEATURE": "Feature",
+    "HOW": "How We Work",
+    "FAQ": "Faq",
+    "PRICELIST": "price list",
+    
+    "TITLEH": "Bringing Your Vision to Life Through Logo Design",
+    "DESCH": "Enhance your brand identity with a professional logo design. We are here to improve your business identity with a quality logo that suits your business needs.",
+    
+    "OPTION": "Logo Options",
+    "FILOSOP": "Logo Philosophy",
+    "LOGOS": "Derived Logo",
+    "MAJOR": "Major Revision",
+    "MINOR": "Minor Revisions",
+    "UNLI": "Unlimited",
+    "BGUID": "Branding Guidelines",
+    "BMATER": "Branding Materials",
+    "PKG": "Packaging Design",
+    "STN": "Stationary Design",
+    "MSS": "Social Media Design",
+    "JSY": "Jersey Design",
+    "MERCH": "Merchandise Design",
+    "BCRD": "Business Card Design",
+    "TWTCH": "Twitch Design Set",
+    "YTUB": "Youtube Design Set",
+    "FBSET": "Facebook Design Set",
+    "TWTER": "Twitter Design Set",
+    "FREEMOCK": "Free MockUp",
+    "MASTER": "Master Files",
+    "PAY": "Receive payment",
+    "CTH": "Design Example",
+    "CTHDESC": "The following is an example of what we have worked on.",
+    
+    "LOGOBRAN": "Personal Brand Logo",
+    "LOGOCOMP": "Company Logo",
+    "LOGOBLOG": "Blogger Logo",
+    "LOGOSTAR": "StartUp Logo",
+    "LOGOUMKM": "MSME Logo",
+    "LOGOESPORT": "Esport Logo",
+    "LOGOFREE": "Free Logo",
+    
+    "LINK": "Link",
+    "SHOP": "Shop",
+    "PORTO": "Portfolio",
+    "TESTI": "Testimonials",
+    "CLIENT": "Our Client",
+    "SERVI": "Service",
+    
+    "SUPP": "Support",
+    "ABOUT": "About",
+    "TERM": "Terms and conditions",
+    "PRIV": "Privacy Policy",
+    "PROG": "Work Process",
+    "HUB": "Reach Us",
+    
+    "JUDULLOGOBRN": "Tarif Desain Logo Personal Brand",
+    "DESCLOGOBRN": "Adalah layanan jasa desain logo profesional yang diperuntukan bagi personal branding yang ingin meningkatkan brandingnya",
+    
+    "JUDULLOGOCOM": "Tarif Desain Logo Perusahaan",
+    "DESCLOGOCOM": "Kami menawarkan berbagai pilihan harga yang fleksibel dan terjangkau. Dapatkan juga tawaran spesial yang memberikan Anda nilai lebih.",
+    
+    "JUDULLOGODGT": "Tarif Desain Logo Digital & StartUp",
+    "DESCLOGODGT": "Kami menawarkan berbagai pilihan harga yang fleksibel dan terjangkau. Dapatkan juga tawaran spesial yang memberikan Anda nilai lebih.",
+    
+    "100": "USD 7$",
+    "150": "USD 10$",
+    "200": "USD 12$",
+    "250": "USD 15$",
+    "350": "USD 25$",
+    "450": "USD 30$",
+    "550": "USD 35$",
+    "650": "USD 40",
+    "700": "USD 45$",
+    "800": "USD 50$",
+    "850": "USD 55$",
+    "950": "USD 65$",
+    "1250": "USD 85$",
+    "2650": "USD 170$",
+    "4850": "USD 350$",
+    "8850": "USD 550$",
+    "FREE": "USD 0$ (FREE)"
+  },
+
+  "zh-tw": {
+    "HOME": "Beranda",
+    "FEATURE": "Fitur",
+    "HOW": "Bagaimana Cara Kerja Kami",
+    "FAQ": "Faq",
+    "PRICELIST": "Daftar Harga",
+    
+    "TITLEH": "Mewujudkan Visi Anda Melalui Desain Logo",
+    "DESCH": "Tingkatkan identitas merek Anda dengan desain logo profesional. Kami hadir untuk meningkatkan identitas bisnis Anda dengan logo berkualitas dan sesuai kebutuhan bisnis anda.",
+    
+    "OPTION": "Opsi Logo",
+    "FILOSOP": "Filosopi Logo",
+    "LOGOS": "Logo Turunan",
+    "MAJOR": "Revisi Besar",
+    "MINOR": "Revisi Kecil",
+    "UNLI": "Tidak Terbatas",
+    "BGUID": "Pedoman Branding",
+    "BMATER": "Material Branding",
+    "PKG": "Desain Packaging",
+    "STN": "Desain Stationary",
+    "MSS": "Desain Media Sosial",
+    "JSY": "Desain Jersey",
+    "MERCH": "Desain Merchandise",
+    "BCRD": "Desain Kartu Nama",
+    "TWTCH": "Set Desain Twitch",
+    "YTUB": "Set Desain Youtube",
+    "FBSET": "Set Desain Facebook",
+    "TWTER": "Set Desain Twitter",
+    "FREEMOCK": "Gratis MockUp",
+    "MASTER": "File Master",
+    "PAY": "Menerima pembayaran",
+    "CTH": "Contoh Desain",
+    "CTHDESC": "Berikut ini adalah contoh yang telah kami kerjakan.",
+    
+    "LOGOBRAN": "Logo Personal Brand",
+    "LOGOCOMP": "Logo Perusahaan",
+    "LOGOBLOG": "Logo Blogger",
+    "LOGOSTAR": "Logo StartUp",
+    "LOGOUMKM": "Logo UMKM",
+    "LOGOESPORT": "Logo Esport",
+    "LOGOFREE": "Logo Gratis",
+    
+    "LINK": "Tautan",
+    "SHOP": "Toko",
+    "PORTO": "Portofolio",
+    "TESTI": "Testimonials",
+    "CLIENT": "Klien Kami",
+    "SERVI": "Layanan",
+    
+    "SUPP": "Support",
+    "ABOUT": "Tentang",
+    "TERM": "Syarat Ketentuan",
+    "PRIV": "Kebijakan pribadi",
+    "PROG": "Proses Pengerjaan",
+    "HUB": "Jangkau Kami",
+    
+    "JUDULLOGOBRN": "Tarif Desain Logo Personal Brand",
+    "DESCLOGOBRN": "Adalah layanan jasa desain logo profesional yang diperuntukan bagi personal branding yang ingin meningkatkan brandingnya",
+    
+    "JUDULLOGOCOM": "Tarif Desain Logo Perusahaan",
+    "DESCLOGOCOM": "Kami menawarkan berbagai pilihan harga yang fleksibel dan terjangkau. Dapatkan juga tawaran spesial yang memberikan Anda nilai lebih.",
+    
+    "JUDULLOGODGT": "Tarif Desain Logo Digital & StartUp",
+    "DESCLOGODGT": "Kami menawarkan berbagai pilihan harga yang fleksibel dan terjangkau. Dapatkan juga tawaran spesial yang memberikan Anda nilai lebih.",
+    
+    "100": "Rp 100.000",
+    "150": "Rp 150.000",
+    "200": "Rp 200.000",
+    "250": "Rp 250.000",
+    "350": "Rp 350.000",
+    "450": "Rp 450.000",
+    "550": "Rp 550.000",
+    "650": "Rp 650.000",
+    "700": "Rp 700.000",
+    "800": "Rp 800.000",
+    "850": "Rp 850.000",
+    "950": "Rp 950.000",
+    "1250": "Rp 1.250.000",
+    "2650": "Rp 2.650.000",
+    "4850": "Rp 4.850.000",
+    "8850": "Rp 8.550.000",
+    "FREE": "Rp 0 (GRATIS)"
+  }
+    };
+
+
+
+ $(document).ready(function () {
+      var lang = localStorage.getItem('lang') || 'en-gb';
+      $(".lang").each(function (index, element) {
+        $(this).text(arrLang[lang][$(this).attr("key")]);
+      });
+
+      $(".translate").click(function () {
+        var lang = $(this).attr("id");
+        localStorage.setItem('lang', lang);
+
+        $(".lang").each(function (index, element) {
+          $(this).text(arrLang[lang][$(this).attr("key")]);
+        });
+      });
+    });
